3594741cfa5063877e14105109a1720aaeda8679
Davide Bulbarelli <bdavide17@gmail.com>
2022-04-01 19:11:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ScoresComponent } from './pages/scores/scores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ScoresComponent
   ],
   imports: [
     BrowserModule,
